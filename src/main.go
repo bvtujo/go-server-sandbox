8cd578431ca666fe391be25e1a9b5221d78a0c3f
bvtujo <austiely@amazon.com>
2020-01-23 15:22:02 -0800

    src reorg and test file

--- src/main.go
+++ src/main.go
@@ -3,14 +3,18 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/", handler)
+	r := mux.NewRouter()
+
+	r.HandleFunc("/hello", handler).Methods("GET")
 
 	http.ListenAndServe(":80", nil)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello world!")
+	fmt.Fprintf(w, "Hello World!")
 }
