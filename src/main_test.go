61a4431ad75288334e656f03636dcd0bee5eaabd
bvtujo <austiely@amazon.com>
2020-01-30 16:42:05 -0800

    Download v0.0.4 and manually set envs in buildspec

--- src/main_test.go
+++ src/main_test.go
@@ -86,5 +86,22 @@ func TestForNonExistentRoute(t *testing.T) {
 }
 
 func TestFileServer(t *testing.T) {
+	r := newRouter()
+	mockServer := httptest.NewServer(r)
 
+	resp, err := http.Get(mockServer.URL + "/assets/")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("Status should be 200, got %d", resp.StatusCode)
+	}
+
+	contentType := resp.Header.Get("Content-Type")
+	expectedContentType := "text/html; charset utf-8"
+
+	if expectedContentType != contentType {
+		t.Errorf("Wrong content type, expected %s got %s", expectedContentType, contentType)
+	}
 }
